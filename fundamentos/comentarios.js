52777b757f88440cacf20af8595b6b5dc80b0596
Pbezerra-dev <herbetyp@gmail.com>
2020-07-31 23:47:12 -0300

    initial commit

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,15 @@
+// Comentario de uma linha 
+console.log('comentário de uma linha')
+
+/*
+Comentario de
+várias linhas
+*/
+console.log('comentário de várias linhas')
+
+/*
+Outro tipo de 
+comentário de
+várias linhas
+*/
+console.log('outro tipo de comentário de várias linhas')
